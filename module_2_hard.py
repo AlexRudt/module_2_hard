31763a62c4fec83912298da2ec5e9010a5f343bb
AlexRudt <Aleksrudt@yandex.ru>
2024-10-08 09:00:32 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,12 @@
+def trap(n):
+    unique = ''
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                unique += str(i) + str(j)
+    return f'{n} - {unique}'
+
+n = int(input())
+
+indiana_jones = trap(n)
+print(indiana_jones)
